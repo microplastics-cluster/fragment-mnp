6f380923fdc6c926cd0460b448563979c5f817ef
Cansu Uluseker <canulu@wlsc-lin38.scicom.ceh.ac.uk>
2026-03-12 17:25:15 +0000

    Updated output prints

--- src/fragmentmnp/output.py
+++ src/fragmentmnp/output.py
@@ -2,11 +2,13 @@
 FRAGMENT-MNP output
 ===================
 
-Provides functionality for processing, summarising, and visualising model
-output data.
+Provides functionality for processing and visulalising model output data.
 
-This file optionally holds additive outputs for multi-additive / multi-pool
-post-processing workflows.
+This file is extended to optionally hold additive outputs:
+- c_chem_part: additive mass concentration in each size class (N x T)
+- c_chem_medium:   additive mass concentration in water (T)
+
+Plotting is extended to optionally plot these additive time series.
 """
 import uuid
 import numpy as np
@@ -16,6 +18,39 @@ import matplotlib.pyplot as plt
 
 class FMNPOutput():
     """
+    Class that holds output data from the model and provides plotting
+    functionalities.
+
+    Parameters
+    ----------
+    t : np.ndarray, shape (n_timesteps,)
+        Time series over which the model was run
+    c: np.ndarray, shape (n_size_classes, n_timesteps)
+        Mass concentrations for each size class over the time
+        series
+    n : np.ndarray, shape (n_size_classes, n_timesteps)
+        Particle number concentrations for each size class over
+        the time series
+    c_diss : np.ndarray, shape (n_timesteps)
+        Total mass concentrations of dissolved organics, including
+        initial concentrations
+    c_min : np.ndarray, shape (n_timesteps)
+        Total mass concentration of mineralised polymer. `c_min` is
+        given as a concentration relative to the original medium
+        that the polymer was present in. For example, if the
+        original medium is soil, and the units are kg/m3, then
+        `c_min` is the mass of mineralised polymer per m3 of the
+        soil, rather than the atmosphere surrounding the soil
+    soln : Bunch object from scipy.integrate.solve_ivp return
+        The solution to the model ODE, passed directly from the
+        scipy.integrate.solve_ivp method
+    psd : np.ndarray, shape (n_size_classes, )
+        Particle size distribution - the average diameters of
+        each of the particle size classes
+    c_chem_part : np.ndarray, shape (n_size_classes, n_timesteps), optional
+        Additive mass concentration in particulate phase, per size class
+    c_chem_medium : np.ndarray, shape (n_timesteps,), optional
+        Additive mass concentration in aqueous phase
     Class that holds output data from the model and provides plotting and
     post-processing utilities.
     """
@@ -43,6 +78,10 @@ class FMNPOutput():
                  c_chem_medium_total=None,
                  additive_names=None,
                  species_names=None) -> None:
+        """
+        Initialise the output data object
+        """
+        # Set the main output data
         self.t = t
         self.c = c
         self.n = n
@@ -50,7 +89,7 @@ class FMNPOutput():
         self.c_min = c_min
         self.soln = soln
         self.psd = psd
-
+        # Optional additive outputs
         self.c_chem_part_species = c_chem_part_species
         self.c_chem_medium_species = c_chem_medium_species
         self.c_chem_part_total = c_chem_part_total
@@ -74,10 +113,15 @@ class FMNPOutput():
             self.c_chem_medium = c_chem_medium_total
             self.A_part = c_chem_part_total
             self.A_aq = c_chem_medium_total
-
+        # Save the number of timesteps and size classes
         self.n_timesteps = self.t.shape[0]
         self.n_size_classes = self.c.shape[0]
-        self.id = uuid.uuid4() if id is None else id
+        # Set the ID based on what we've been given, or
+        # give a unique ID
+        if id is None:
+            self.id = uuid.uuid4()
+        else:
+            self.id = id
 
     # ------------------------------------------------------------------
     # Basic helpers
@@ -333,14 +377,16 @@ class FMNPOutput():
         except ImportError as exc:
             raise ImportError('size_class_contribution_dataframe() requires pandas.') from exc
         return pd.DataFrame(self.size_class_contribution_records(level=level))
+    
 
-    # ------------------------------------------------------------------
-    # Plotting helpers for additive post-processing
-    # ------------------------------------------------------------------
+    # -----------------
+    # Plotting 
+    # -----------------
     def plot(self,
              type: str = 'mass_conc',
              plot_dissolution: bool = False,
              plot_mineralisation: bool = False,
+             # NEW:
              plot_additive: bool = False,
              additive_log_yaxis=False,
              log_yaxis=False,
@@ -350,11 +396,73 @@ class FMNPOutput():
              size_classes_to_plot=None,
              additive_index: int | None = None,
              show: bool = False):
+        """
+        Plot the output data by choosing from a number of
+        pre-defined plot types.
+
+        Parameters
+        ----------
+        type : str, default='mass_conc'
+            Tells the function what type of plot to produce.
+            Either `particle_number_conc` or `mass_conc`.
+        plot_dissolution : bool, default=False
+            Should dissolution be plotted on a separate y-axis
+        plot_mineralisation : bool, default=False
+            Should mineralised polymer be plotted on a separate y-axis
+        plot_additive : bool, default=False
+            If True and additive outputs are present, plot additive trajectories.
+            - particulate additive (per size class) uses same colormap scheme
+            - aqueous additive is plotted on a twin axis (dashed black)
+        additive_log_yaxis : bool or str, default=False
+            log scaling for additive axis (similar options as log_yaxis)
+        log_yaxis : bool or str, default=False
+            True and "log" plots the y axis on a log scale,
+            "symlog" plots the y axis on a symlog scale (useful
+            if the data contain zeros), and False plots the y
+            axis on a linear scale
+        units : dict or str, default=None
+            Units to be used in axis labels. Must either be "SI" to
+            use SI units, "dim" to use dimensional labels ("mass",
+            "volume", etc), or a dictionary containing elements for
+            `mass`, `volume` and `time`. E.g. `units={'mass': 'kg',
+            'volume': 'm3', 'time': 's', 'length': 'nm'}`. Note that
+            these units are used purely for axis labelling and are
+            not used to modify the model output data (which is unit
+            agnostic). If `None` (the default), no units are added to
+            labels.
+        cmap : str, default='viridis'
+            The colormap to use for the plot. Must be one of the
+            colormaps `available in matplotlib
+            <https://matplotlib.org/stable/gallery/color/colormap_reference.html>`_.
+            Note that these are case-sensitive.
+        show_legend : bool, default=True
+            Should size classes be shown on a legend?
+        size_classes_to_plot : list of ints, default=None
+            Only plot specific size classes by providing a list of
+            size class indices to plot, where 0 is the index of the
+            smallest size class. By default, all size classes are
+            plotted.
+        show : bool, default=False
+            Should ``plt.show()`` be called in order to trigger the
+            plot to show in non-interactive environments (e.g.
+            running the code as a script)? If `False`, the plot will
+            not be shown automatically unless you are using an
+            interactive environment, such as a Jupyter notebook.
+
+        Returns
+        -------
+        (matplotlib.figure.Figure, matplotlib.axes.Axes)
+            Matplotlib figure and axes objects for the plot
+        """
         unit_labels = self._construct_units(units)
+        # Are we plotting number or mass concentrations? Set the
+        # y values and labels accordingly
         if type == 'particle_number_conc':
+            # Set the yaxis label
             ylabel = 'Particle number concentration'
             if unit_labels is not None:
                 ylabel += f' [{unit_labels["number_conc"]}]'
+            # Set the yaxis values, transposed to pass to mpl
             yvals = self.n.T
         elif type == 'mass_conc':
             ylabel = 'Mass concentration'
@@ -362,44 +470,61 @@ class FMNPOutput():
                 ylabel += f' [{unit_labels["mass_conc"]}]'
             yvals = self.c.T
         else:
+            # Raise an error if an invalid plot type is given
             raise ValueError(f'Invalid option for plot `type`: {type}. '
-                             'Should be `particle_number_conc` or `mass_conc`.')
-
+                             'Should be `particle_number_conc` or'
+                             '`mass_conc`.')
+        # Only plot the size classes we have been asked to
         if size_classes_to_plot is not None:
             yvals = yvals[:, size_classes_to_plot]
-
+        # Set the xaxis label
         xlabel = 'Time'
         if unit_labels is not None:
             xlabel += f' [{unit_labels["time"]}]'
 
+        # Change the colourmap to something useful (Viridis)
         cmap_ = plt.colormaps[cmap]
-        plt.rcParams['axes.prop_cycle'] = plt.cycler(
-            'color', cmap_(np.linspace(0, 1, yvals.shape[1]))
-        )
+        plt.rcParams['axes.prop_cycle'] = \
+            plt.cycler('color',
+                       cmap_(np.linspace(0, 1, yvals.shape[1])))
 
+        # Create the figure and axes - we need to this because we need to
+        # add a second axis for the dissolution data
         fig, ax1 = plt.subplots()
+
+        # Construct the xlabel with(out) units
+        xlabel = 'Time'
+        if unit_labels is not None:
+            xlabel += f' [{unit_labels["time"]}]'
+        # Add the x and y labels
         ax1.set_xlabel(xlabel)
         ax1.set_ylabel(ylabel)
-
+        # Should the y axis be logged?
         if log_yaxis in [True, 'log']:
             ax1.set_yscale('log')
         elif log_yaxis == 'symlog':
             ax1.set_yscale('symlog')
 
+        # Plot the concentrations
         ax1.plot(self.t, yvals)
 
+        # Add a size class legend, if requested
         if show_legend:
+            # Construct the legend with(out) units
             legend = [f'{d:<1g}' for d in self.psd]
             if unit_labels is not None:
                 legend = [f'{sc} {unit_labels["length"]}' for sc in legend]
             legend = np.array(legend)
+            # Only show the requested size classes on the legend
             if size_classes_to_plot is not None:
                 legend = legend[size_classes_to_plot]
             ax1.legend(legend)
 
-        ax2 = None
+        ax2 = None  # NEW: ensure ax2 always exists, even if we don't plot dissolution/mineralisation
+        # Create and format the dissolution y axis
         if plot_dissolution or plot_mineralisation:
             ax2 = ax1.twinx()
+            # Construct the ylabel
             if not plot_mineralisation:
                 ylabel_diss = 'Dissolved mass concentration'
                 legend = ['Dissolved']
@@ -416,12 +541,21 @@ class FMNPOutput():
             if unit_labels is not None:
                 ylabel_diss += f' [{unit_labels["mass_conc"]}]'
             ax2.set_ylabel(ylabel_diss)
+            # Always show the dissolution legend to make it clear
+            # which line is dissolution
             ax2.legend(legend)
+            # Should the y axis be logged?
             if log_yaxis in [True, 'log']:
                 ax2.set_yscale('log')
             elif log_yaxis == 'symlog':
                 ax2.set_yscale('symlog')
+            ax_ = (ax1, ax2)
+        else:
+            ax_ = ax1
 
+        # -----------------------------
+        # 3) NEW: Optional additive plotting
+        # -----------------------------
         ax3 = None
         if plot_additive:
             if self.c_chem_part is None or self.c_chem_medium is None:
